462b4fb1ea24216905a19325944feb9421d75b26
Himanshu Verma <hims6482@gmail.com>
2023-07-16 13:55:28 +0530

    readme fixes

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://Chit-Chat.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://chit-chat-a5ky.onrender.com:5000"; // "https://Chit-Chat.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
